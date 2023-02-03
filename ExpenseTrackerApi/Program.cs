5fff905414795883edaf569080e88e1183ad73bd
Joseph Izang <josephizang@gmail.com>
2023-02-03 02:08:56 +0100

    fix and update project with nuget packages

--- ExpenseTrackerApi/Program.cs
+++ ExpenseTrackerApi/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddDbContext<ExpenseTrackerContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("RemotePgsql"));
 });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
