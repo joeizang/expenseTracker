4c2527ead9d0c828fb127d38c2241b5436dd7c29
Joseph Izang <josephizang@gmail.com>
2023-02-04 01:07:00 +0100

    add migrations to handle concurrency conflicts in db

--- ExpenseTrackerApi/Migrations/20230204000458_HandleConcurrencyInDb.Designer.cs
+++ ExpenseTrackerApi/Migrations/20230204000458_HandleConcurrencyInDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExpenseTrackerApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExpenseTrackerApi.Migrations
 {
     [DbContext(typeof(ExpenseTrackerContext))]
-    partial class ExpenseTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20230204000458_HandleConcurrencyInDb")]
+    partial class HandleConcurrencyInDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ExpenseTrackerApi/Migrations/20230204000458_HandleConcurrencyInDb.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTrackerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class HandleConcurrencyInDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                table: "ExpenseTypes",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                table: "Expenses",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                table: "ExpenseTypes");
+
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                table: "Expenses");
+        }
+    }
+}
